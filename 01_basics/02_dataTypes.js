dbb287e6fbeea0fe0fca4180532f1363153ef5a2
Vivek Gujar <135510786+VivekGujar45@users.noreply.github.com>
2025-09-03 11:04:36 +0000

    data types done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+// alert (3+3) we are using nodejs not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Vivek")
+
+let name = "vivek"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number = 2 tp power 53
+// bigint
+// string = " "
+// boolean = true/false
+// null = standlone value
+// undefined = no value assign
+// symbol = unique
+
+
+// object
+
+console.log(typeof "vivek");
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
