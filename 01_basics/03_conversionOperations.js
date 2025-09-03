5b62a80ef4a4a20fc9388d93a19a1c6b83c1df86
Vivek Gujar <135510786+VivekGujar45@users.noreply.github.com>
2025-09-03 11:51:49 +0000

    converison updated

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,4 +1,4 @@
-let score = //true//undefined //null //"33abc" -> INPUT_TYPES
+/*let score = //true//undefined //null //"33abc" -> INPUT_TYPES
 
 console.log(typeof score);
 console.log(typeof (score));// get the data type of the value
@@ -28,4 +28,41 @@ let someNumber=45
 
 let stringNumber = String(someNumber)
 console.log(stringNumber);
-console.log(typeof stringNumber);
+console.log(typeof stringNumber);*/
+
+// ------------------OPERATIONS---------------------
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);*/
+
+let str1=10
+let str2=2
+
+let str3=str1+str2
+console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+3+"2");
+
+console.log(+true);
+console.log(+"");
+
+let n1, n2, n3
+n1=n2=n3 =2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
