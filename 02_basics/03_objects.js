2f1ec26d07305697b685da19cf4c3b273a7c99a9
Vivek Gujar <135510786+VivekGujar45@users.noreply.github.com>
2025-09-04 13:16:34 +0000

    object done

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,32 @@
+// singleton
+// 
+
+// object literals
+
+const mySym=Symbol("key1")
+
+const JSuser = {
+    name: "vivek",
+    "fullname": "vivekgujar",
+    [mySym]: "mykey1",
+    age: 22,
+    location: "PCMC",
+    email: "vg@google.com",
+    isLoggedin: false,
+    lastLoginDays: ["mon","sat"]
+}
+
+// console.log(JSuser.email); // dot deke hi aap call kr skate ho object ko
+// console.log(JSuser["email"]); // sidhe bracket se bhi kar sakte ho
+// console.log(JSuser.fullname);
+// console.log(JSuser[mySym]);
+// console.log(typeof JSuser[mySym]);
+
+JSuser.email = "vivekgujar@chatgpt.com" // ovrwrites the object element
+//Object.freeze(JSuser)
+JSuser.email = "vivekguj@zomato.com"
+
+console.log(JSuser);
+
+
+
