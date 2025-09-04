702678bc2d815b693e20c93282a7edf5c171d9f5
Vivek Gujar <135510786+VivekGujar45@users.noreply.github.com>
2025-09-04 07:28:08 +0000

    revised datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+//  Primitive
+
+//  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+console.log(typeof heros); // Type is Object
+
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+myFunction()
+console.log(typeof myFunction); //data type is Function
+
+
+console.log(typeof anotherId); // type is symbol
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
