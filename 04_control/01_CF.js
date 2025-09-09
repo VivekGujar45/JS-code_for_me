6bc67bf63efe4617c0025bb17b19040e499df70c
Vivek Gujar <135510786+VivekGujar45@users.noreply.github.com>
2025-09-09 07:13:20 +0000

    basics of CF

---
+++ 04_control/01_CF.js
@@ -0,0 +1,33 @@
+// if
+
+// const userLoggedIn = true
+// marks=52
+// if (marks >=50) {
+//     console.log("gretaer than 50");
+    
+// } else{
+//     console.log("less than 50");
+    
+// }
+// console.log("false");
+
+
+// >,<,<=,>=,=(assign karto),==(equal ahe ka check krto),===(equal ahe ka check krto with data type)
+// !=, !==
+
+// const score =200
+
+// if (score>100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+    
+// }
+// console.log(`User power: ${power}`);// not defined scope issue
+
+// const balance =1000
+
+// if (balance >500) console.log("test"), console.log("test2");
+// ;
+
+const balance =1000
+
