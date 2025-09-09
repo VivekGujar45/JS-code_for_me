8b6b375e16b04a5610ddaa733a9f39f8982f729f
Vivek Gujar <135510786+VivekGujar45@users.noreply.github.com>
2025-09-09 12:06:24 +0000

    for in and of loops done

---
+++ 05_iterations/04_loops.js
@@ -0,0 +1,17 @@
+const myObject = {
+    js :'javascript',
+    cpp : 'C++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+for (const key in myObject) {
+    // console.log(myObject[key]);
+    
+}
+
+const programming = ['js','cpp','java','python']
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
