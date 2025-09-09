eefd9a600c0ac914b08fd7f12fc306249ae5dce6
Vivek Gujar <135510786+VivekGujar45@users.noreply.github.com>
2025-09-09 11:21:52 +0000

    while loops doe

---
+++ 05_iterations/02_loops.js
@@ -0,0 +1,23 @@
+
+let i=0
+while (i<=10) {
+    // console.log(`value of index is ${i}`);
+    
+    i=i+2
+}
+
+let myArray= ["spiddy","ironman","superman"]
+let arr=0
+
+while (arr<myArray.length) {
+    // console.log(`value of Myarray is ${myArray[arr]}`);
+    arr=arr+1
+}
+
+let score = 11
+do {
+    // console.log(`Score is: ${score}`);
+    score++
+} while (score <=10);
+
+// put loop on arrays
